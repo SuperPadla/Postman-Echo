c14bb6f0a5cc49f5388ffb2be192c36a77f12f8b
Ксения Сажнева <13pokemon@mail.ru>
2022-09-22 17:13:52 +0300

    other files

---
+++ src/test/java/ru/netology/unit/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPost() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("BlaBlaBla") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("BlaBlaBla"))
+        ;
+    }
+}
